91436512b18696180926bf3c397bd9a1b70841d5
Sam Wizard <sam.wizard@gmail.com>
2019-05-18 08:03:26 +0100

    express server rewrite

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
-const express = require("express");
-const serveStatic = require("serve-static")
+const express = require('express');
+
+const serveStatic = require("serve-static");
+
 const path = require('path');
 
-const db = require("/models");
-const apiDriver = require("/app/api/driver");
-const apiTeam = require("/app/api/team");
-
 const app = express();
-app.use(bodyParser.json());
 
-apiDriver(app, db);
-apiTeam(app, db);
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/driver', apiDriver);
-
-app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to the beginning of nothingness.',
-}));
-
-app.use(serveStatic(path.join(__dirname, '/dist')));
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 app.listen(port);
