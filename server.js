d24da3e1cb236acd064aed654e9c1e3e75fa558e
Sam Wizard <sam.wizard@gmail.com>
2019-05-18 08:16:44 +0100

    t

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const path = require('path');
 
 const app = express();
 
+app.get('/get', function (req, res) {
+    res.send('GET request to the homepage')
+})
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 80;
